9bfc6d98d5575d39ecda6eacd2764ec84b01bf9d
sabbir <sabbir@gmail.com>
2023-03-11 22:16:04 +0600

    login function

--- contollers/userController.js
+++ contollers/userController.js
@@ -41,60 +41,28 @@ module.exports.register = async (req, res, next) => {
         next(err);
     }
 };
-// module.exports.register = async (req, res, next) => {
-
-//     try {
-//         const { userName, email, password } = req.body;
-
-//         const CheckUserName = await User.findOne({ userName });
-//         const CheckUserEmail = await User.findOne({ email });
-
-//         if (CheckUserName) {
-//             return res.json({ message: "Username Already Exit, Try Another One.", status: false });
-//         }
-//         if (CheckUserEmail) {
-//             return res.json({ message: "User  Email Already Exit, Try Another One.", status: false });
-//         }
-
-//         const hashedPassword = await bcrypt.hash(password, 10);
-
-//         const user = {
-//             userName,
-//             email,
-//             password: hashedPassword
-//         };
-
-//         const result = await User.create(user);
-
-//         delete user.password;
-
-//         return res.json({ user, status: true });
-//     }
-//     catch (err) {
-//         next(err);
-//         console.log(err);
-//     }
-
-// };
 
 //login controll
 module.exports.login = async (req, res, next) => {
     try {
 
-        const { userName, email, password } = req.body;
-
-        const user = await User.findOne({ userName });
-        if (!user) {
-            return res.json({ status: false, message: "Invalid user name or password " });
-        }
-
-        const passwordValidation = await bcrypt.compare(password, user.password);
-        if (!passwordValidation) {
-            return res.json({ status: false, message: "Invalid user name or password " });
-        }
-        delete user.password;
-        return res.json({ status: true, user });
     }
+
+
+    //     const { userName, email, password } = req.body;
+
+    //     const user = await User.findOne({ userName });
+    //     if (!user) {
+    //         return res.json({ status: false, message: "Invalid user name or password " });
+    //     }
+
+    //     const passwordValidation = await bcrypt.compare(password, user.password);
+    //     if (!passwordValidation) {
+    //         return res.json({ status: false, message: "Invalid user name or password " });
+    //     }
+    //     delete user.password;
+    //     return res.json({ status: true, user });
+    // }
     catch (error) {
         next(error);
     }
